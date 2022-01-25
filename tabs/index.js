08df81f667f9765e4f2f419d8aa0b5a7cb12958d
Akshay <akshaybhamare10@gmail.com>
2022-01-25 15:37:54 +0530

    Added hoverable tabs

--- tabs/index.js
+++ tabs/index.js
@@ -1,67 +1,63 @@
 // Get all tab nodes
 const horizontalTabs = document.querySelectorAll('.horizontal .tab');
 const verticalTabs = document.querySelectorAll('.vertical .tab');
+const hoverTabs = document.querySelectorAll('.hover .tab');
+
 // Get content node to fill tab information
 const h_content = document.querySelector('.horizontal .content');
 const v_content = document.querySelector('.vertical .content');
+const hover_content = document.querySelector('.hover .content');
 
-function renderContent(country, info, type) {
-    let title, text;
-
-    if (type === 'hTab') {
-        title = h_content.firstElementChild;
-        text = h_content.lastElementChild;  
-    } else {
-        title = v_content.firstElementChild;
-        text = v_content.lastElementChild;  
-    }
+function renderContent(country, info, element) {
+    let title = element.firstElementChild;
+    let text = element.lastElementChild;  
     
     title.textContent = country;
     text.textContent = info;
 }
 
-function updateActive(type) {
-    let currActive;
-    if (type === 'hTab') {
-        currActive = document.querySelector('.horizontal .active');
-    } else {
-        currActive = document.querySelector('.vertical .active');
-    }
-    currActive.classList.remove('active');
-}
-
-function onTabEvent(event, type) {
-    // remove current 'active' tab class
-    updateActive(type);
-
+function onTabEvent(event, contentElement) {
     switch (event.currentTarget.textContent) {
         case 'London':
             event.currentTarget.classList.add('active');
-            renderContent('London', 'London is the capital city of England.', type);
+            renderContent('London', 'London is the capital city of England.', contentElement);
             return;
         case 'Paris':
             event.currentTarget.classList.add('active');
-            renderContent('Paris', 'Paris is the capital of France.', type);
+            renderContent('Paris', 'Paris is the capital of France.', contentElement);
             return;
         case 'New York':
             event.currentTarget.classList.add('active');
-            renderContent('New York', 'New York is the capital of USA.', type);
+            renderContent('New York', 'New York is the capital of USA.', contentElement);
             return;
         default:
             return;
     }
 }
 
-// Add 'click' event to each tabList nodes
+// Add 'click' event to each horizontalTabs nodes
 horizontalTabs.forEach(function (tab) {
     tab.addEventListener('click', function (e) {
-        onTabEvent(e, 'hTab');
+        // remove current 'active' tab class
+        document.querySelector('.horizontal .active').classList.remove('active');
+        onTabEvent(e, h_content);
     });
 });
 
-// Add 'click' event to each tabList nodes
+// Add 'click' event to each verticalTabs nodes
 verticalTabs.forEach(function (tab) {
     tab.addEventListener('click', function (e) {
-        onTabEvent(e, 'vTab');
+        // remove current 'active' tab class
+        document.querySelector('.vertical .active').classList.remove('active');
+        onTabEvent(e, v_content);
+    });
+});
+
+// Add 'mouseover' event to each hoverTabs nodes
+hoverTabs.forEach(function (tab) {
+    tab.addEventListener('mouseover', function (e) {
+        // remove current 'active' tab class
+        document.querySelector('.hover .active').classList.remove('active');
+        onTabEvent(e, hover_content);
     });
 });
