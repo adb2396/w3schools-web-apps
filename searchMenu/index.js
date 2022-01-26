2c648117e6d0d041a4bd6276a8cb4dc6d6a82bac
Akshay <akshaybhamare10@gmail.com>
2022-01-26 21:32:48 +0530

    Created search menu template

---
+++ searchMenu/index.js
@@ -0,0 +1,8 @@
+// Get search input node
+const search = document.getElementById('search');
+// list of menu items
+const menuList = document.querySelector('.menu li');
+
+search.addEventListener('change', function (e) {
+    const { value } = e.currentTarget;
+});
