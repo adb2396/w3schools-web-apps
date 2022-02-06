78b8ce2977864a0643c5fa3fc98edd2510ee360f
Akshay <akshaybhamare10@gmail.com>
2022-02-06 12:26:09 +0530

    Search Menu - Added list filter

--- searchMenu/index.js
+++ searchMenu/index.js
@@ -1,8 +1,16 @@
 // Get search input node
 const search = document.getElementById('search');
 // list of menu items
-const menuList = document.querySelector('.menu li');
+const menuList = document.querySelectorAll('.menu li');
 
-search.addEventListener('change', function (e) {
-    const { value } = e.currentTarget;
+search.addEventListener('keyup', function (e) {
+    const value = e.currentTarget.value.toLowerCase();
+
+    menuList.forEach(function (item) {
+        if (item.textContent.toLowerCase().includes(value)) {
+            item.style.display = "";
+        } else {
+            item.style.display = "none"
+        }
+    });
 });
